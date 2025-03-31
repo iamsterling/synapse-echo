130890144a1ab8d2cb72c68102cae1b146b52780
sterling holt <jsterlingholt@me.com>
2025-03-30 22:33:35 -0500

    ...

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+    // ...other configuration settings...
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // completely disables the rule
+      // ...other rules...
+    },
+};
