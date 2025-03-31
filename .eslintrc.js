43784742c49da910a25cc0a25bcc107feb58e9bd
sterling holt <jsterlingholt@me.com>
2025-03-31 10:03:21 -0500

    reconciling merge conflict

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-// .eslintrc.js
-module.exports = {
-    // ...other configuration settings...
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // completely disables the rule
-      // ...other rules...
-    },
-};
